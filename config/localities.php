7fc6b35105ba4aedb48dd17a7ad5d7febc961a4e
Carlos_23 <123482213+CarlosCH23@users.noreply.github.com>
2025-10-04 17:32:48 +0200

    Add files via upload

---
+++ config/localities.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'bogota' => [
+        'Antonio Nariño',
+        'Barrios Unidos',
+        'Bosa',
+        'Chapinero',
+        'Ciudad Bolívar',
+        'Candelaria',
+        'Engativá',
+        'Fontibón',
+        'Kennedy',
+        'La Candelaria',
+        'Los Mártires',
+        'Puente Aranda',
+        'Rafael Uribe Uribe',
+        'San Cristóbal',
+        'Santa Fe',
+        'Sumapaz',
+        'Suba',
+        'Teusaquillo',
+        'Tunjuelito',
+        'Usaquén',
+    ],
+];
